508ea5019a9449950fadd02b00cd528fa61c0c93
Ingemar Finn <ingemar.finn@king.com>
2022-12-02 12:25:36 +0100

    Add css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Calculator from "./Calculator";
+import './index.css'
 
 function App() {
     return (
