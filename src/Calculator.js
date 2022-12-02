5de87b6724213dd49fed3e8e0bb044a68ace8a51
Ingemar Finn <ingemar.finn@king.com>
2022-12-02 12:28:35 +0100

    Remove header buttons

--- src/Calculator.js
+++ src/Calculator.js
@@ -353,39 +353,6 @@ class Calculator extends Component {
                     </div>
                 </div>
 
-<!--
-                <div className="save-section py-5">
-                    <div className="row pb-3">
-                        <div className="col-sm">
-                            <p><i className="far fa-save"/> <strong>Best recipe? Save it!</strong></p>
-                            <label htmlFor="nameInput">Name:</label>
-                            <div className="input-group mb-3">
-                                <input type="text"
-                                       className="form-control"
-                                       id="nameInput"
-                                       value={this.state.name}
-                                       onChange={this.onNameInputChange}
-                                />
-                            </div>
-                            <button type="button"
-                                    onClick={this.handleSave}
-                                    className="btn btn-primary saveButton">Save recipe!
-                            </button>
-                        </div>
-                    </div>
-                    {this.state.saveResult &&
-                    <div className="row">
-                        <div className="col-sm">
-                            <div className="alert alert-success" role="alert">
-                                <strong>{this.state.saveResult}</strong> {this.state.redirectUrl}
-                            </div>
-                        </div>
-                    </div>
-                    }
-                </div>
--->
-
-
                 <Modal show={this.state.showModal} onHide={this.handleClose}>
                     <Modal.Header closeButton>
                         <Modal.Title>Change ingredients</Modal.Title>
